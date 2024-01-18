cbd73be40c403a500a8be2fbd4632a2f13708542
Vladimir Barakhtyanskiy <barahtasurgut@gmail.com>
2024-01-18 16:55:23 +0500

    main

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+
+
+  return (
+      <div className="App">
+          <div className="bestblock">
+              <div className="bestblock_space">
+                  <div className="bestblock_space_text">Hello</div>
+                  <div className="bestblock_space_text">Hello</div>
+              </div>
+          </div>
+          <div className="bestblock_square"></div>
+      </div>
+  );
+}
+
+export default App;
